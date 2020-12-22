3f849437de7352e6b07419ef1786cdb4ca057420
Jorge Venegas <jorgepvenegas@protonmail.com>
2020-12-22 14:38:16 -0800

    lil comma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import {ChakraProvider, extendTheme,  } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 
 const globalTheme = {
