4df9cd4ebd5542abed78fb353e5a2a6484b421c9
Jorge Venegas <jorgepvenegas@protonmail.com>
2020-12-23 15:20:35 -0800

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,48 @@
+import MagicWordForm from "../components/QuestionForm";
+import { useRouter } from 'next/router'
+import { Text, Heading, Flex, Box } from "@chakra-ui/react";
 import { NextPage } from "next";
-import NextLink from "next/link";
-import { Heading, Link, Flex, Image, Text } from "@chakra-ui/react";
 
 const IndexPage: NextPage = () => {
   const { NEXT_PUBLIC_USERNAME } = process.env;
-
+  const router = useRouter();
 
   return (
-    <Flex bg="red.300" flexDirection="column" alignItems="center">
-      <Heading as="h1" size="xl">Hey {NEXT_PUBLIC_USERNAME}! No peeking.</Heading>
-      <Heading as="h2" size="lg">Ok?</Heading>
-      <Image src="https://media.giphy.com/media/ELoRBat8ZCTSM/giphy.gif" alt="Gif 1" />
-      <Text fontSize="md">The elves are hard at work. Come back on December 25th.</Text>
-      <Image src="https://media.giphy.com/media/MrxXXBriEIKBO/giphy.gif" alt="Gif 2" />
-      <NextLink href="/questions" passHref>
-        <Link>Questions</Link>
-      </NextLink>
+    <Flex flexDirection="column" alignItems="center" marginTop={20}>
+      <Box
+        flexDirection="column"
+        alignItems="center"
+        w="3xl"
+        className="question-container">
+        <Box
+          w="100%"
+          minHeight={700}
+          padding={7}
+          borderRadius={10}
+          bg="white"
+          align="center"
+          marginBottom={10}>
+          <Heading as="h1" size="xl">
+            Hi {NEXT_PUBLIC_USERNAME}! Welcome!
+          </Heading>
+          <Box marginTop="40">
+            <Box width="70%">
+              <Text>Oh hello there! Enter the magic word</Text>
+              <MagicWordForm
+                color="red"
+                questionName="magicWord"
+                answer={process.env.NEXT_PUBLIC_TOKEN}
+                errorMessage="Not so fast! That's incorrect."
+                buttonText={"Let's Go!"}
+                onSuccess={() => {
+                  router.push(`/questions`);
+                }} />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </Flex>
-  )
-}
+  );
+};
 
 export default IndexPage;
